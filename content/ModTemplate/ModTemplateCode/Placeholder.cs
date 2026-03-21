e7f0658a020cbada96cbc735f2ed87db1105e8f1
Alchyr <isithalchyr@gmail.com>
2026-03-20 23:12:13 -0700

    empty mod template adjustment

---
+++ content/ModTemplate/ModTemplateCode/Placeholder.cs
@@ -0,0 +1,6 @@
+﻿namespace ModTemplate.ModTemplateCode;
+
+public class Placeholder
+{
+    //You're recommended but not required to keep all your code in this package and all your assets in the ModTemplate folder.
+}
