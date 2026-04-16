477433217a5407caf51261100ddad4bc219fe42e
Eclarift <noreply@eclarift.com>
2026-04-16 19:35:14 +0000

    cleanup again

--- del/content/CharacterModTemplate/CharModCode/Potions/CharModPotion.cs
+++
@@ -1,8 +0,0 @@
-﻿using BaseLib.Abstracts;
-using BaseLib.Utils;
-using CharMod.CharModCode.Character;
-
-namespace CharMod.CharModCode.Potions;
-
-[Pool(typeof(CharModPotionPool))]
-public abstract class CharModPotion : CustomPotionModel;
